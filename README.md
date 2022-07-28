727c75ee8ed61710b83c8426d12bc600b303aa92
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2022-07-28 20:39:20 +0100

    feat: Completed the get user using API calls.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Link to the postman documentation
 
 <https://documenter.getpostman.com/view/10977711/UzXPxGMN>
+
+## Run the command below to seed the users into the database
+
+php artisan migrate:fresh --seed
+
+## Endpoints
+
+Get User: {{base_url}}/api/v1/user/{id}
--- app/Http/Controllers/api/v1/UserController.php
+++ app/Http/Controllers/api/v1/UserController.php
@@ -3,9 +3,49 @@
 namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    //Function to get user
+    public function getUser($id){
+        try {
+
+                $user = new User();
+
+                //Checking if an id exist
+                $userObject = $user::where('id', $id)->first();
+
+                if ($userObject) {
+                    $user = User::find($id);
+                    return response()->json([
+                        "status" => "true",
+                        "message" => "User found",
+                        "data" => $user
+                    ]);
+                }
+                else {
+                    return response()->json([
+                        "status" => "false",
+                        "message" => "User not found",
+                        "data" => []
+                    ], 404);
+                }
+
+
+
+
+        }  catch (\Exception $exception){
+            return response()->json([
+                "status" => "false",
+                "message" => "Something went wrong.",
+                "data" => []
+            ], 500);
+        }
+
+    }
+
+
 }
